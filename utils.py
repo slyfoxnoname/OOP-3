a1d826c9698aceb627f02d84313b0a38f2b0ffee
Yuliia Petrova <yuliiatrova@gmail.com>
2024-02-18 18:11:28 +0200

    Add prime_num to utils.py
    Add prime_num func to utils.py with pull request

--- utils.py
+++ utils.py
@@ -23,3 +23,16 @@ def prime_num(n):
     else:
       print("Введене число НЕ є простим")
 
+# чи є число простим (add with pull request)
+
+def prime_num(n):
+    is_prime = True
+    for i in range (2, (n//2) + 1):
+        if n % i == 0:
+            is_prime = False
+        break
+    if is_prime:
+        print("Введене число є простим")
+    else:
+      print("Введене число НЕ є простим")
+
