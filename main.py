6551792a4e0482c5afa3739f1e2cbb65615a8734
Egor Orlik <egororlik27@knu,ua>
2024-02-18 16:00:08 +0200

    add cod

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import utils
-#виклик функції степеня п'ятірки
-num = int(input())
-print(utils.is_power_of_five(num))
+
+n = int(input())
+print(utils.factorial(n))
--- utils.py
+++ utils.py
@@ -1,16 +1,11 @@
-def power_five(num):
-    degree = 0
-    while 5 ** degree <= num:
-        if 5 ** degree == num:
-            return True
-        degree += 1
-
-    return False
-
-number = int(input("Введіть число: "))
-result = power_five(number)
-
-if result:
-    print(f"{number} є степенем п'ятірки.")
-else:
-    print(f"{number} не є степенем п'ятірки.")
+#factorial func
+def factorial(n):
+    fact = 1
+    if n < 0:
+        raise ValueError("Please, input integer >= 0")
+    elif n == 0:
+        return 1
+    else:
+        for i in range(1, n + 1):
+            fact = fact * i
+        print(fact)
