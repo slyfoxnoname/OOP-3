8cc045e51161528f634017ea22208b3f59ed36e7
Yuliia Petrova <yuliiatrova@gmail.com>
2024-02-18 17:35:26 +0200

    Add prime_num func to utils.py

--- utils.py
+++ utils.py
@@ -9,3 +9,17 @@ def factorial(n):
         for i in range(1, n + 1):
             fact = fact * i
         print(fact)
+
+#prime numbers (added without pull request)
+
+def prime_num(n): 
+    is_prime = True
+    for i in range (2, (n//2) + 1):
+        if n % i == 0:
+            is_prime = False
+        break
+    if is_prime:
+        print("Введене число є простим")
+    else:
+      print("Введене число НЕ є простим")
+
